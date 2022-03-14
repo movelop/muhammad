f8d26a44f1e804d3659655c60ab6f3975ec5c95e
Babalola Muhammad <barmilitary@yahoo.com>
2022-03-14 15:05:37 +0100

    header fixed

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
         whileInView = { scaleVariants.whileInView }
         className = 'app__header-circles'
       >
-        {[images.redux, images.react, images.sass].map((circle, index) => (
+        {[images.node, images.react, images.redux].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="profile_bg" />
           </div>
